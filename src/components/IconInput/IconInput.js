601bf5ae1e8e8b59aedbd81d4332a3b98849c179
Ivan Vrsajkov <vrsajkov.ivan@gmail.com>
2024-05-26 22:00:56 +0200

    add support for two sizes

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,12 +13,50 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
+  const SIZE_STYLES = {
+    small: {
+      borderWidth: 1,
+      iconSize: 16,
+      iconStrokeWidth: 1,
+      totalHeight: 24,
+      fontSize: 14,
+      lineHeight: 16,
+      padding: 4,
+      iconTextSpacing: 8,
+    },
+    large: {
+      borderWidth: 2,
+      iconSize: 24,
+      iconStrokeWidth: 2,
+      totalHeight: 36,
+      fontSize: 18,
+      lineHeight: 21,
+      padding: 6,
+      iconTextSpacing: 12,
+    }
+  }
+
+  const sizeStyle = SIZE_STYLES[size];
+
+  if (!sizeStyle) {
+    throw new Error(`Size not supported: ${size}`);
+  }
+
   return (
-    <InputWrapper style={{ "--width": width + "px" }}>
-      <IconWrapper>
-        <Icon id={icon} size={16} />
+    <InputWrapper style={{
+      "--height": sizeStyle.totalHeight + "px",
+      "--width": width + "px",
+      "--border-width": sizeStyle.borderWidth + "px"
+    }}
+    >
+      <IconWrapper style={{ "--icon-size": sizeStyle.iconSize + "px" }}>
+        <Icon id={icon} size={sizeStyle.iconSize} strokeWidth={sizeStyle.iconStrokeWidth} />
       </IconWrapper>
-      <TextInput placeholder={placeholder} />
+      <TextInput placeholder={placeholder} style={{
+        "--padding": (sizeStyle.padding + "px ").repeat(3) + (sizeStyle.iconSize + sizeStyle.iconTextSpacing) + "px",
+        "--font-size": (sizeStyle.fontSize / 16) + "rem",
+      }}
+      />
     </InputWrapper>
   )
 };
@@ -26,11 +64,12 @@ const IconInput = ({
 const IconWrapper = styled.div`
   position: absolute;
 
-  height: 16px;
+  height: var(--icon-size);
 
-  top: 4px;
-  bottom: 4px;
+  top: 0;
+  bottom: 0;
   left: 0;
+  margin: auto;
 `;
 
 const TextInput = styled.input.attrs({
@@ -42,13 +81,15 @@ const TextInput = styled.input.attrs({
   left: 0;
   right: 0;
 
-  padding: 4px 4px 4px 24px;
+  padding: var(--padding);
 
   border: none;
 
   background-color: transparent;
   color: ${COLORS.gray700};
+
   font-weight: 700;
+  font-size: var(--font-size);
 
   &::placeholder {
       color: ${COLORS.gray500};
@@ -59,10 +100,10 @@ const TextInput = styled.input.attrs({
 const InputWrapper = styled.div`
   position: relative;
 
-  height: 24px;
+  height: var(--height);
   width: var(--width);
 
-  border-bottom: 1px solid ${COLORS.black}
+  border-bottom: var(--border-width) solid ${COLORS.black}
 `;
 
 export default IconInput;
