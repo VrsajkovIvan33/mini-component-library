6c7e71eb9ea715b4cb10b19d9b515bc4f0f9e5fa
Ivan Vrsajkov <vrsajkov.ivan@gmail.com>
2024-05-26 21:12:04 +0200

    add initial structure with underline, icon and input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,52 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  // return <TextInput />;
+  return (
+    <InputWrapper>
+      <Underline />
+      <IconWrapper>
+        <Icon id="search" size={16} />
+      </IconWrapper>
+      <TextInput />
+    </InputWrapper>
+  )
 };
+
+const Underline = styled.div`
+  height: 24px;
+  background-color: transparent;
+  border-bottom: 1px solid ${COLORS.black}
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+
+  height: 16px;
+  width: 16px;
+
+  top: 4px;
+  bottom: 4px;
+  left: 0;
+`;
+
+const TextInput = styled.input.attrs({
+  type: "text"
+})`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  padding: 4px 4px 4px 24px;
+
+  border: none;
+  background-color: transparent;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+`;
 
 export default IconInput;
