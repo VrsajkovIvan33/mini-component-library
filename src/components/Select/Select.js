2a122a9add827f6eef7c64cabdeeaef96c1993cc
Ivan Vrsajkov <vrsajkov.ivan@gmail.com>
2024-05-26 17:24:02 +0200

    add custom chevron icon and hide original one

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,36 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <ContainingBlockWrapper>
+      <SelectWrapper>
+        {displayedValue}
+        <Icon id="chevron-down" style={{ display: "inline-block" }}></Icon>
+      </SelectWrapper>
+      <HiddenSelect value={value} onChange={onChange}>
+        {children}
+      </HiddenSelect>
+    </ContainingBlockWrapper>
   );
 };
+
+const ContainingBlockWrapper = styled.div`
+  position: relative;
+`;
+
+const HiddenSelect = styled.select`
+  // Cover the entire parent area
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  // Make the element invisible but still present (and responding to clicks)
+  opacity: 0;
+`;
+
+const SelectWrapper = styled.div`
+
+`;
 
 export default Select;
